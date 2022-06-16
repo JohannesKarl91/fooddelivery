5d0234dae5712ab0fe0afa17b4c9af37094afca4
Johannes Weber <johannes.k.weber@web.de>
2022-06-16 16:43:49 +0200

    Added some more dishes to the global variable

--- src/app/global-variables/food-menu.global-variable.ts
+++ src/app/global-variables/food-menu.global-variable.ts
@@ -1,77 +1,140 @@
 export const foodMenu: Array<any> = [{
-    name: 'Hamburger',
-    text: 'Reines Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Ketchup & Senf',
-    category: 'Burger',
-    price: 7.60,
-    amount: 1
+  name: 'Chicken Wings BBQ oder HOT',
+  text: 'mit Ranch Dressing',
+  category: 'Apetizer',
+  price: 4.90,
+  amount: 1
 },
 {
-    name: 'Cheeseburger',
-    text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Cheddar Käse, Ketchup & Senf',
-    category: 'Burger',
-    price: 7.90,
-    amount: 1
+  name: 'Zwiebelringe',
+  text: 'mit Sauerrahm',
+  category: 'Apetizer',
+  price: 4.90,
+  amount: 1
 },
 {
-    name: 'Farmburger',
-    text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Speck, BBQ Sauce, Cheddar Käse, Ketchup & Senf',
-    category: 'Burger',
-    price: 8.50,
-    amount: 1
-  },
-  {
-    name: 'Chili Burger',
-    text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Chili con Carne, Ketchup',
-    category: 'Burger',
-    price: 9.50,
-    amount: 1
-  },
-  {
-    name: 'Italian Burger',
-    text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Chili con Carne, Ketchup',
-    category: 'Burger',
-    price: 9.50,
-    amount: 1
-  },
-  {
-    name: 'Pulled Pork Burger',
-    text: 'Geschmorte Schweineschulter mit BBQ-Sauce, hausgemachter Krautsalat & Blattsalat',
-    category: 'Burger',
-    price: 9.90,
-    amount: 1
-  },
-  {
-    name: 'Chicken Fried Burger',
-    text: 'Fritiertes Hühnchenfilet, Tomaten, Salat, Gurken & Mayonnaise',
-    category: 'Burger',
-    price: 7.90,
-    amount: 1
-  },
-  {
-    name: 'Fish Burger',
-    text: 'Kabeljau Filet, Salat, Zwiebeln, Cheddar Käse & hausgemachte Remoulade',
-    category: 'Burger',
-    price: 9.30,
-    amount: 1
-  },
-  {
-    name: 'Double Cheese Burger',
-    text: '2 x 120g Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Cheddar Käse, Ketchup & Senf',
-    category: 'Burger',
-    price: 9.80,
-    amount: 1
-  },
-  {
-    name: 'Hawaii Burger',
-    text: 'Paniertes Hühnchenfilet, Kochschinken, Ananas, Cheddar Käse & Curry Sauce',
-    category: 'Burger',
-    price: 9.40,
-    amount: 1
-  },
-  {
-    name: 'French Fries',
-    text: 'Erlesene, hausgemachte & fritierte Kartoffeln aus der Region',
-    category: 'Beilage',
-    price: 2.90,
-    amount: 1
-  }]
+  name: 'Nachos',
+  text: 'mit Salsa Dip',
+  category: 'Apetizer',
+  price: 3.50,
+  amount: 1
+},
+{
+  name: 'Frittierte Morzarella Sticks',
+  text: 'mit Salsa Dip',
+  category: 'Apetizer',
+  price: 6.50,
+  amount: 1
+},
+{
+  name: 'Potato Wedges',
+  text: 'frittierte Kartoffelecken mit Sauerrahm & Schnittlauch',
+  category: 'Apetizer',
+  price: 3.80,
+  amount: 1
+},
+{
+  name: 'Hamburger',
+  text: 'Reines Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Ketchup & Senf',
+  category: 'Burger',
+  price: 7.60,
+  amount: 1
+},
+{
+  name: 'Cheeseburger',
+  text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Cheddar Käse, Ketchup & Senf',
+  category: 'Burger',
+  price: 7.90,
+  amount: 1
+},
+{
+  name: 'Farmburger',
+  text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Speck, BBQ Sauce, Cheddar Käse, Ketchup & Senf',
+  category: 'Burger',
+  price: 8.50,
+  amount: 1
+},
+{
+  name: 'Chili Burger',
+  text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Chili con Carne, Ketchup',
+  category: 'Burger',
+  price: 9.50,
+  amount: 1
+},
+{
+  name: 'Italian Burger',
+  text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Chili con Carne, Ketchup',
+  category: 'Burger',
+  price: 9.50,
+  amount: 1
+},
+{
+  name: 'Pulled Pork Burger',
+  text: 'Geschmorte Schweineschulter mit BBQ-Sauce, hausgemachter Krautsalat & Blattsalat',
+  category: 'Burger',
+  price: 9.90,
+  amount: 1
+},
+{
+  name: 'Chicken Fried Burger',
+  text: 'Fritiertes Hühnchenfilet, Tomaten, Salat, Gurken & Mayonnaise',
+  category: 'Burger',
+  price: 7.90,
+  amount: 1
+},
+{
+  name: 'Fish Burger',
+  text: 'Kabeljau Filet, Salat, Zwiebeln, Cheddar Käse & hausgemachte Remoulade',
+  category: 'Burger',
+  price: 9.30,
+  amount: 1
+},
+{
+  name: 'Double Cheese Burger',
+  text: '2 x 120g Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Cheddar Käse, Ketchup & Senf',
+  category: 'Burger',
+  price: 9.80,
+  amount: 1
+},
+{
+  name: 'Hawaii Burger',
+  text: 'Paniertes Hühnchenfilet, Kochschinken, Ananas, Cheddar Käse & Curry Sauce',
+  category: 'Burger',
+  price: 9.40,
+  amount: 1
+},
+{
+  name: 'French Fries',
+  text: 'Erlesene, hausgemachte & fritierte Kartoffeln aus der Region',
+  category: 'Beilage',
+  price: 2.90,
+  amount: 1
+},
+{
+  name: 'Milkshake',
+  text: 'mit Schokolade und Sahne',
+  category: 'Dessert',
+  price: 3.50,
+  amount: 1
+},
+{
+  name: 'Milchshake',
+  text: 'mit Vanille und Sahne',
+  category: 'Dessert',
+  price: 3.50,
+  amount: 1
+},
+{
+  name: 'Hot Brownie Sundae',
+  text: 'Haugemachter Brownie serviert mit Vanilleeis, Sahne & Schokosoße',
+  category: 'Dessert',
+  price: 5.90,
+  amount: 1
+},
+{
+  name: 'Devils Food Cake',
+  text: 'Schokokkuchen, serviert mit Vanilleeis, Schlagsahne & Schokosauce',
+  category: 'Dessert',
+  price: 3.90,
+  amount: 1
+}]
