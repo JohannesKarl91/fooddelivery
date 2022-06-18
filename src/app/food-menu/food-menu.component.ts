b12b5bc605ec9bfdf7b9d2033b619b4c9982ced3
Johannes Weber <johannes.k.weber@web.de>
2022-06-18 23:39:19 +0200

    Creating header part and finished first section

--- src/app/food-menu/food-menu.component.ts
+++ src/app/food-menu/food-menu.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit, Inject } from '@angular/core';
   styleUrls: ['./food-menu.component.scss']
 })
 export class FoodMenuComponent implements OnInit {
+  grades: Array<any> = [
+    'star_black.svg', 'star_black.svg',
+    'star_black.svg',
+    'star_half_black.svg',
+    'star_border_black.svg'
+  ];
+
+  conditions: Array<any> = [
+    { logo: 'av_timer_black.svg', value: '33 - 50 min' },
+    { logo: 'pedal_bike_black.svg', value: '0.50 €' },
+    { logo: 'shopping_cart_black.svg', value: 'Min. 10,00 €' }
+  ];
 
   constructor(@Inject('FOODMENU') public foodMenu: any[]) { }
 
