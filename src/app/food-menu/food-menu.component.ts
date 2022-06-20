c1b6edafcb09d6087ef8d838a5708f8db13f9dc5
Johannes Weber <johannes.k.weber@web.de>
2022-06-20 15:23:35 +0200

    Implementing info- & favorite-button to food-menu-header section

--- src/app/food-menu/food-menu.component.ts
+++ src/app/food-menu/food-menu.component.ts
@@ -12,12 +12,13 @@ export class FoodMenuComponent implements OnInit {
     'star_half_black.svg',
     'star_border_black.svg'
   ];
-
   conditions: Array<any> = [
     { logo: 'av_timer_black.svg', value: '33 - 50 min' },
     { logo: 'pedal_bike_black.svg', value: '0.50 €' },
     { logo: 'shopping_cart_black.svg', value: 'Min. 10,00 €' }
   ];
+  favoriteBtn: Boolean = false;
+
 
   constructor(@Inject('FOODMENU') public foodMenu: any[]) { }
 
@@ -25,4 +26,23 @@ export class FoodMenuComponent implements OnInit {
     console.log('All items of FOODMENU in "food-menu.component.ts" are:', this.foodMenu)
   }
 
+
+  changeIntoFavorite() {
+
+    if (this.favoriteBtn == false) {
+      let favoriteButtonUnset = document.getElementById('button-favorite-unset');
+      let favoriteButtonSet = document.getElementById('button-favorite-set');
+      favoriteButtonUnset?.classList.add('d-none');
+      favoriteButtonSet?.classList.remove('d-none');
+      this.favoriteBtn = true;
+    }
+
+    else {
+      let favoriteButtonUnset = document.getElementById('button-favorite-unset');
+      let favoriteButtonSet = document.getElementById('button-favorite-set');
+      favoriteButtonUnset?.classList.remove('d-none');
+      favoriteButtonSet?.classList.add('d-none');
+      this.favoriteBtn = false;
+    }
+  }
 }
