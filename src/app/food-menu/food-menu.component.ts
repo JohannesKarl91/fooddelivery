135c58036a74a1698131411b3fdc2f4a5e70141f
Johannes Weber <johannes.k.weber@web.de>
2022-06-21 20:12:46 +0200

    Added search field into food-menu-navbar

--- src/app/food-menu/food-menu.component.ts
+++ src/app/food-menu/food-menu.component.ts
@@ -27,22 +27,50 @@ export class FoodMenuComponent implements OnInit {
   }
 
 
+  //Via onclick the user can mark the restaurant as a favorite one. 
   changeIntoFavorite() {
-
     if (this.favoriteBtn == false) {
-      let favoriteButtonUnset = document.getElementById('button-favorite-unset');
-      let favoriteButtonSet = document.getElementById('button-favorite-set');
-      favoriteButtonUnset?.classList.add('d-none');
-      favoriteButtonSet?.classList.remove('d-none');
-      this.favoriteBtn = true;
+      this.setFavoriteRestaurant();
     }
-
     else {
-      let favoriteButtonUnset = document.getElementById('button-favorite-unset');
-      let favoriteButtonSet = document.getElementById('button-favorite-set');
-      favoriteButtonUnset?.classList.remove('d-none');
-      favoriteButtonSet?.classList.add('d-none');
-      this.favoriteBtn = false;
+      this.removeFavoriteRestaurant();
     }
   }
+
+
+  setFavoriteRestaurant() {
+    let favoriteButtonUnset = document.getElementById('button-favorite-unset');
+    let favoriteButtonSet = document.getElementById('button-favorite-set');
+    favoriteButtonUnset?.classList.add('d-none');
+    favoriteButtonSet?.classList.remove('d-none');
+    this.favoriteBtn = true;
+  }
+
+
+  removeFavoriteRestaurant() {
+    let favoriteButtonUnset = document.getElementById('button-favorite-unset');
+    let favoriteButtonSet = document.getElementById('button-favorite-set');
+    favoriteButtonUnset?.classList.remove('d-none');
+    favoriteButtonSet?.classList.add('d-none');
+    this.favoriteBtn = false;
+  }
+
+
+  openSearchMode() {
+    let searchMode = document.getElementById('search-mode');
+    searchMode?.classList.remove('d-none');
+    searchMode?.classList.add('d-flex');
+    let startMode = document.getElementById('start-mode');
+    startMode?.classList.add('d-none');
+  }
+
+
+  closeSearchMode(){
+    let searchMode = document.getElementById('search-mode');
+    searchMode?.classList.add('d-none');
+    searchMode?.classList.remove('d-flex');
+    let startMode = document.getElementById('start-mode');
+    startMode?.classList.remove('d-none');
+  }
+
 }
