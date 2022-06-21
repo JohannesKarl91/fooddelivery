4a98ef0beb218bb508809c43373d07daf6f07573
Johannes Weber <johannes.k.weber@web.de>
2022-06-21 12:35:42 +0200

    Adding navbar in food-menu-section

--- src/app/global-variables/food-menu.global-variable.ts
+++ src/app/global-variables/food-menu.global-variable.ts
@@ -36,70 +36,70 @@ export const foodMenu: Array<any> = [{
 {
   name: 'Hamburger',
   text: 'Reines Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Ketchup & Senf',
-  category: 'Burger',
+  category: 'Classic Burger',
   price: 7.60,
   amount: 1
 },
 {
   name: 'Cheeseburger',
   text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Cheddar Käse, Ketchup & Senf',
-  category: 'Burger',
+  category: 'Classic Burger',
+  price: 7.90,
+  amount: 1
+},
+{
+  name: 'Double Cheese Burger',
+  text: '2 x 120g Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Cheddar Käse, Ketchup & Senf',
+  category: 'Classic Burger',
+  price: 9.80,
+  amount: 1
+},
+{
+  name: 'Chicken Fried Burger',
+  text: 'Fritiertes Hühnchenfilet, Tomaten, Salat, Gurken & Mayonnaise',
+  category: 'Classic Burger',
   price: 7.90,
   amount: 1
 },
 {
   name: 'Farmburger',
   text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Speck, BBQ Sauce, Cheddar Käse, Ketchup & Senf',
-  category: 'Burger',
+  category: 'Classic Burger',
   price: 8.50,
   amount: 1
 },
 {
   name: 'Chili Burger',
   text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Chili con Carne, Ketchup',
-  category: 'Burger',
+  category: 'Classic Burger',
   price: 9.50,
   amount: 1
 },
 {
   name: 'Italian Burger',
   text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Chili con Carne, Ketchup',
-  category: 'Burger',
+  category: 'Gourmet Burger',
   price: 9.50,
   amount: 1
 },
 {
   name: 'Pulled Pork Burger',
   text: 'Geschmorte Schweineschulter mit BBQ-Sauce, hausgemachter Krautsalat & Blattsalat',
-  category: 'Burger',
+  category: 'Gourmet Burger',
   price: 9.90,
-  amount: 1
-},
-{
-  name: 'Chicken Fried Burger',
-  text: 'Fritiertes Hühnchenfilet, Tomaten, Salat, Gurken & Mayonnaise',
-  category: 'Burger',
-  price: 7.90,
   amount: 1
 },
 {
   name: 'Fish Burger',
   text: 'Kabeljau Filet, Salat, Zwiebeln, Cheddar Käse & hausgemachte Remoulade',
-  category: 'Burger',
+  category: 'Gourmet Burger',
   price: 9.30,
-  amount: 1
-},
-{
-  name: 'Double Cheese Burger',
-  text: '2 x 120g Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Cheddar Käse, Ketchup & Senf',
-  category: 'Burger',
-  price: 9.80,
   amount: 1
 },
 {
   name: 'Hawaii Burger',
   text: 'Paniertes Hühnchenfilet, Kochschinken, Ananas, Cheddar Käse & Curry Sauce',
-  category: 'Burger',
+  category: 'Gourmet Burger',
   price: 9.40,
   amount: 1
 },
@@ -108,6 +108,13 @@ export const foodMenu: Array<any> = [{
   text: 'Erlesene, hausgemachte & fritierte Kartoffeln aus der Region',
   category: 'Beilage',
   price: 2.90,
+  amount: 1
+},
+{
+  name: 'Potato Wedges',
+  text: 'frittierte Kartoffelecken mit Sauerrahm & Schnittlauch',
+  category: 'Beilage',
+  price: 3.80,
   amount: 1
 },
 {
