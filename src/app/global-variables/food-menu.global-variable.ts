14d77dbea29f6963220d13d5d9537a136101a1c8
Johannes Weber <johannes.k.weber@web.de>
2022-06-28 15:48:33 +0200

    Added apetizers to food-menu-list

--- src/app/global-variables/food-menu.global-variable.ts
+++ src/app/global-variables/food-menu.global-variable.ts
@@ -5,28 +5,32 @@ export const foodMenuFavorite: Array<any> = [
     text: 'mit Salsa Dip',
     category: 'Favorites',
     price: 6.50,
-    amount: 1
+    amount: 1,
+    id: 3
   },
   {
     name: 'Cheeseburger',
     text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Cheddar Käse, Ketchup & Senf',
     category: 'Favorites',
     price: 7.90,
-    amount: 1
+    amount: 1,
+    id: 6
   },
   {
     name: 'Pulled Pork Burger',
     text: 'Geschmorte Schweineschulter mit BBQ-Sauce, hausgemachter Krautsalat & Blattsalat',
     category: 'Favorites',
     price: 9.90,
-    amount: 1
+    amount: 1,
+    id: 12
   },
   {
     name: 'Devils Food Cake',
     text: 'Schokokkuchen, serviert mit Vanilleeis, Schlagsahne & Schokosauce',
     category: 'Favorites',
     price: 3.90,
-    amount: 1
+    amount: 1,
+    id: 20
   }]
 
 
@@ -37,35 +41,40 @@ export const foodMenuApetizer: Array<any> = [
     text: 'mit Ranch Dressing',
     category: 'Apetizer',
     price: 4.90,
-    amount: 1
+    amount: 1,
+    id: 0
   },
   {
     name: 'Zwiebelringe',
     text: 'mit Sauerrahm',
     category: 'Apetizer',
     price: 4.90,
-    amount: 1
+    amount: 1,
+    id: 1
   },
   {
     name: 'Nachos',
     text: 'mit Salsa Dip',
     category: 'Apetizer',
     price: 3.50,
-    amount: 1
+    amount: 1,
+    id: 2
   },
   {
     name: 'Frittierte Morzarella Sticks',
     text: 'mit Salsa Dip',
     category: 'Apetizer',
     price: 6.50,
-    amount: 1
+    amount: 1,
+    id: 3
   },
   {
     name: 'Potato Wedges',
     text: 'frittierte Kartoffelecken mit Sauerrahm & Schnittlauch',
     category: 'Apetizer',
     price: 3.80,
-    amount: 1
+    amount: 1,
+    id: 4
   }]
 
 
@@ -76,42 +85,48 @@ export const foodMenuClassicburger: Array<any> = [
     text: 'Reines Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Ketchup & Senf',
     category: 'Classic Burger',
     price: 7.60,
-    amount: 1
+    amount: 1,
+    id: 5
   },
   {
     name: 'Cheeseburger',
     text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Cheddar Käse, Ketchup & Senf',
     category: 'Classic Burger',
     price: 7.90,
-    amount: 1
+    amount: 1,
+    id: 6
   },
   {
     name: 'Double Cheese Burger',
     text: '2 x 120g Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Cheddar Käse, Ketchup & Senf',
     category: 'Classic Burger',
     price: 9.80,
-    amount: 1
+    amount: 1,
+    id: 7
   },
   {
     name: 'Chicken Fried Burger',
     text: 'Fritiertes Hühnchenfilet, Tomaten, Salat, Gurken & Mayonnaise',
     category: 'Classic Burger',
     price: 7.90,
-    amount: 1
+    amount: 1,
+    id: 8
   },
   {
     name: 'Farmburger',
     text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Speck, BBQ Sauce, Cheddar Käse, Ketchup & Senf',
     category: 'Classic Burger',
     price: 8.50,
-    amount: 1
+    amount: 1,
+    id: 9
   },
   {
     name: 'Chili Burger',
     text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Chili con Carne, Ketchup',
     category: 'Classic Burger',
     price: 9.50,
-    amount: 1
+    amount: 1,
+    id: 10
   }]
 
 
@@ -122,28 +137,32 @@ export const foodMenuGourmetburger: Array<any> = [
     text: 'Rindfleisch, Tomaten, Salat, Zwiebeln, Gurken, Chili con Carne, Ketchup',
     category: 'Gourmet Burger',
     price: 9.50,
-    amount: 1
+    amount: 1,
+    id: 11
   },
   {
     name: 'Pulled Pork Burger',
     text: 'Geschmorte Schweineschulter mit BBQ-Sauce, hausgemachter Krautsalat & Blattsalat',
     category: 'Gourmet Burger',
     price: 9.90,
-    amount: 1
+    amount: 1,
+    id: 12
   },
   {
     name: 'Fish Burger',
     text: 'Kabeljau Filet, Salat, Zwiebeln, Cheddar Käse & hausgemachte Remoulade',
     category: 'Gourmet Burger',
     price: 9.30,
-    amount: 1
+    amount: 1,
+    id: 13
   },
   {
     name: 'Hawaii Burger',
     text: 'Paniertes Hühnchenfilet, Kochschinken, Ananas, Cheddar Käse & Curry Sauce',
     category: 'Gourmet Burger',
     price: 9.40,
-    amount: 1
+    amount: 1,
+    id: 14
   }]
 
 
@@ -154,14 +173,16 @@ export const foodMenuSidedish: Array<any> = [
     text: 'Erlesene, hausgemachte & fritierte Kartoffeln aus der Region',
     category: 'Side dish',
     price: 2.90,
-    amount: 1
+    amount: 1,
+    id: 15
   },
   {
     name: 'Potato Wedges',
     text: 'frittierte Kartoffelecken mit Sauerrahm & Schnittlauch',
     category: 'Side dish',
     price: 3.80,
-    amount: 1
+    amount: 1,
+    id: 16
   }]
 
   
@@ -172,28 +193,32 @@ export const foodMenuDessert: Array<any> = [
     text: 'mit Schokolade und Sahne',
     category: 'Dessert',
     price: 3.50,
-    amount: 1
+    amount: 1,
+    id: 17
   },
   {
     name: 'Milchshake',
     text: 'mit Vanille und Sahne',
     category: 'Dessert',
     price: 3.50,
-    amount: 1
+    amount: 1,
+    id: 18
   },
   {
     name: 'Hot Brownie Sundae',
     text: 'Haugemachter Brownie serviert mit Vanilleeis, Sahne & Schokosoße',
     category: 'Dessert',
     price: 5.90,
-    amount: 1
+    amount: 1,
+    id: 19
   },
   {
     name: 'Devils Food Cake',
     text: 'Schokokkuchen, serviert mit Vanilleeis, Schlagsahne & Schokosauce',
     category: 'Dessert',
     price: 3.90,
-    amount: 1
+    amount: 1,
+    id: 20
   }]
 
 
